fe3463d53032ef2e8f83ff5a08c2afd70122bd37
YonisHussein <yonismhussein@gmail.com>
2022-06-17 20:32:59 -0400

    additional additions

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const PORt = process.env.PORT || 3001;
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/assets/index.html'));
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
